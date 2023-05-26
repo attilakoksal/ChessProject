55d8c116cb6d1d6296bb788b1cc9b23789aaee88
Nishant Tiwari <ntiwa006@xe-01.cs.ucr.edu>
2023-05-25 21:52:09 -0700

    Fixed Cmake

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/csmajs/ntiwa006/final-project-rmoha023-ntiwa006-akoks002-swang433-1/ChessBoard.cpp" "CMakeFiles/test.dir/ChessBoard.cpp.o" "gcc" "CMakeFiles/test.dir/ChessBoard.cpp.o.d"
+  "/home/csmajs/ntiwa006/final-project-rmoha023-ntiwa006-akoks002-swang433-1/ChessSquare.cpp" "CMakeFiles/test.dir/ChessSquare.cpp.o" "gcc" "CMakeFiles/test.dir/ChessSquare.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/csmajs/ntiwa006/final-project-rmoha023-ntiwa006-akoks002-swang433-1/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
