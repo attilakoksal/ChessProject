d7c202ec627d991331cc9c010944410a7806c916
attilakoksal <attila.b.koksal@gmail.com>
2023-06-04 01:34:21 -0700

    is valid working on

--- isValidQueen.cpp
+++ isValidQueen.cpp
@@ -12,6 +12,38 @@ bool ChessBoard::isValidQueen(ChessSquare *currentSquare, ChessSquare *targetSqu
     int directionX;
     int directionY;
 
-    
+    // check if the current position of the queen is different than the target position wanted for the queen
+    // if it's the same position, invalid move
+    if(currentQueenX == newQueenX && currentQueenY == newQueenY){
+        return false;
+    }
+
+    // check horizontal or vertical movement
+    if (currentQueenX == newQueenX || currentQueenY == newQueenY){
+        int begin;
+        int end;
+        int addByOne;
+
+        if(currentQueenX == newQueenX){
+            begin = currentQueenY;
+            end = newQueenY;
+        }
+        else{
+            begin = currentQueenX;
+            end = newQueenX;
+        }
+
+        if(end > begin){
+            addByOne = 1;
+        }
+        else{
+            addByOne = -1;
+        }
+
+        for(int i = begin + addByOne; i != end; i+=addByOne){
+            
+        }
+
+    }
 
 }
