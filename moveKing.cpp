eb349c1a23e7ccdd99cc46f5a4bda5e750f642fa
attilakoksal <attila.b.koksal@gmail.com>
2023-06-05 11:51:04 -0700

    moveKing with capture done

--- moveKing.cpp
+++ moveKing.cpp
@@ -2,21 +2,37 @@
 
 using namespace std;
 
-// directly modifies the board by updating the king's positions and the target square that the king is wanted to go
-// isValidKing checks if the movement by the king is valid or not, and if it's valid, then the movement is enacted
-// if the movement is invalid, no changes are made to the board, other than an error message
+void moveKing(int oldx, int oldy, int newx, int newy) {
+    ChessSquare* oldSquare = getSquare(oldx, oldy);
+    ChessSquare* newSquare = getSquare(newx, newy);
 
-void ChessBoard::moveKing(ChessSquare* currentSquare, ChessSquare* targetSquare){
-
-    if(isValidKing(currentSquare,targetSquare)){
-        targetSquare->setSquare(currentSquare);
-        currentSquare->setEmpty();
-
-        currentSquare = targetSquare; // updating position
-    }
-    else{
-        cout << "Invalid move. Try again." << endl;
+    if (!oldSquare->isOccupied() || (newSquare->isOccupied() && isupper(oldSquare->getPiece()[0]) == isupper(newSquare->getPiece()[0]))) {
+        cout << "Invalid move, try again." << endl;
         return;
     }
 
+    // Check for valid king movement
+    if (abs(oldx - newx) > 1 || abs(oldy - newy) > 1) {
+        cout << "Invalid move, try again." << endl << endl;
+        return;
+    }
+
+    // Check if the destination square is occupied by a piece of the opposite color
+    if (newSquare->isOccupied() && isupper(oldSquare->getPiece()[0]) != isupper(newSquare->getPiece()[0])) {
+        cout << "Capturing " << newSquare->getPiece() << endl;
+    }
+
+    // Move the king to the new square
+    newSquare->setPiece(oldSquare->getPiece());
+    newSquare->setOccupied(true);
+    oldSquare->setPiece(" ");
+    oldSquare->setOccupied(false);
+
+    // Update the board representation
+    board[oldy][oldx]->setPiece(" ");
+    board[oldy][oldx]->setOccupied(false);
+    board[newy][newx]->setPiece("K");
+    board[newy][newx]->setOccupied(true);
+
+    cout << endl << "King moved from " << char(oldx + 'a') << 8 - oldy << " to " << char(newx + 'a') << 8 - newy << endl << endl;
 }
